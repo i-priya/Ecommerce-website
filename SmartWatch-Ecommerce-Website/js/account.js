9fb1ccd32c343a4f4bdec0e8118520d620554196
Priyadarshini <122440222+i-priya@users.noreply.github.com>
2023-01-11 15:23:04 +0530

    Add files via upload

---
+++ SmartWatch-Ecommerce-Website/js/account.js
@@ -0,0 +1,25 @@
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const loginLink = document.querySelector("form .login-link a");
+const signupLink = document.querySelector("form .signup-link a");
+
+
+
+signupBtn.onclick = (() => {
+	loginForm.style.marginLeft = "-50%";
+	loginText.style.marginLeft = "-50%";
+});
+loginBtn.onclick = (() => {
+	loginForm.style.marginLeft = "0%";
+	loginText.style.marginLeft = "0%";
+});
+signupLink.onclick = (() => {
+	signupBtn.click();
+	return false;
+});
+loginLink.onclick = (() => {
+	loginBtn.click();
+	return false;
+});
